547788936c179553d0dfd86546612d2d241a8bdd
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2022-03-29 20:21:34 +0200

    setup script

---
+++ setp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gautocorr',
+      version='1.0',
+      author='Julius Bier Kirkegaard',
+      py_modules=['gautocorr'],
+     )
