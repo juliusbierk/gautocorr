577243aa5e56404d13fd246e4dde4ace90f8e879
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2022-03-29 20:23:22 +0200

    remove wrong file

--- setp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='gautocorr',
-      version='1.0',
-      author='Julius Bier Kirkegaard',
-      py_modules=['gautocorr'],
-     )
